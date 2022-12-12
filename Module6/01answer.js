4bdf457c62b44102ec7b4ae3f01e43aed219fea1
gabbypaola <35908028+gabbypaola@users.noreply.github.com>
2022-12-11 22:12:01 -0500

    Mod 6

--- Module6/01answer.js
+++ Module6/01answer.js
@@ -1,25 +1,20 @@
 d3.csv('ue_industry.csv', data => {
 
-    // Define your scales and generator here.
-    console.log(data);
-
     const xScale = d3.scaleLinear()
         .domain(d3.extent(data, d => +d.index))
-        .range([1180, 20]);
+        .range([20, 1180]);
 
     const yScale = d3.scaleLinear()
         .domain(d3.extent(data, d => +d.Agriculture))
         .range([580, 20]);
 
-    var line = d3.line()
+    const line = d3.line()
         .x(d => xScale(+d.index))
-        .y(d => yScale(+d['Agriculture']))
-        .curve(d3.curveCardinal);
+        .y(d => yScale(+d.Agriculture));
 
     d3.select('#answer1')
 		.append('path')
 		.attr('d', line(data))
-		.attr('stroke', 'red')
-        // append more elements here
+		.attr('stroke', '474a4f');
 
 });
